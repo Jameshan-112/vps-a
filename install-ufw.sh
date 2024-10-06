74488d893e63e3f428b4306b8cd3b263e4ef0e2a
Jameshan-112 <130420401+Jameshan-112@users.noreply.github.com>
2024-10-06 10:55:46 +0800

    Update install-ufw.sh

--- install-ufw.sh
+++ install-ufw.sh
@@ -3,16 +3,15 @@ apt update
 apt upgrade -y
 # 更新包列表
 apt update
-
 # 安装 UFW 防火墙
 apt install -y ufw
 
 # 默认拒绝所有传入连接
 ufw default deny
-
+sleep 1
 # 允许 SSH 端口 (22)
 ufw allow 22
-
+sleep 1
 # 启用 UFW 防火墙
 ufw enable
 
